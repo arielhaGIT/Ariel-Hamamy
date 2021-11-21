2b62d37c8edd0eca7dcd37180c055dc894e58105
arielhaGIT <arielha@post.bgu.ac.il>
2021-11-21 10:54:38 +0200

    class4

---
+++ EX5_JS.js
@@ -0,0 +1,5 @@
+function MyName(){
+    document.getElementById("Button").innerHTML= "Thank you!";
+    console.log("Ariel Hamamy");
+}
+
---
+++ animation.js
@@ -0,0 +1,15 @@
+function thankyou(){
+    document.getElementById("Button").innerHTML= "Thank You";
+    console.log("thank you");
+}
+
+function changePic(){
+    document.getElementById("HelAnimation").src="beach2.jpg";
+}
+
+var x=5, y=7, t=(x==y);
+var text=(x==y);
+console.log(text)
+
+
+
