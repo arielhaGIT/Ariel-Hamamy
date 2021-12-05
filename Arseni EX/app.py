4cfb7cb117bcfb726be69eff34cb3eae72dd089d
arielhaGIT <arielha@post.bgu.ac.il>
2021-12-05 20:11:23 +0200

    base

--- Arseni EX/app.py
+++ Arseni EX/app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World! my name is Ariel'
 
+@app.route('/about', methods=['GET', 'POST'])
+def about_func():
+    #TODO
+    #do something with DB
+    return redirect(url_for('profile_func'))
+
+
+@app.route('/profile')
+def profile_func():
+    return 'Welcome to profile page'
+
+@app.route('/cart')
+def cart_func():  # put application's code here
+    #TODO
+    #DO SOMTHING WITH DB
+    return redirect('/profile')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
--- Class/app.py
+++ Class/app.py
@@ -11,12 +11,18 @@ def hello_world():  # put application's code here
 def about_func():
     #TODO
     #do something with DB
-    #return redirect(url_for('catalog_func'))
-    return 'welcome to about'
+    return redirect(url_for('catalog_func'))
+
 
 @app.route('/catalog')
 def catalog_func():
     return 'Welcome to catalog page'
+
+@app.route('/US')
+def US_func():  # put application's code here
+    #TODO
+    #DO SOMTHING WITH DB
+    return redirect('/catalog')
 
 if __name__ == '__main__':
     app.run(debug=True)
